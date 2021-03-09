99cb3303c29263625980325253ad836e1d719a57
hamid <hamidroohi71@gmail.com>
2021-03-09 15:52:26 +0330

    - Set the request to do once

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import './App.css';
-import React from 'react';
-import UserPart from './Components/UsersPart';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import UserPart from "./Components/UsersPart";
 
 function App() {
-   return(
-     <UserPart />
-   )
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    //Requesting the api to recieve the users data
+    fetch("https://reqres.in/api/users?page=1")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.data);
+        },
+        (error) => {
+          setIsLoaded(isLoaded);
+          setError(error);
+        }
+      );
+  },[]);
   
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else return <UserPart data={items} />;
 }
 
 export default App;
